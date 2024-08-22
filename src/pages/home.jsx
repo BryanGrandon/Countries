f0cdaed3e22cd0413e50e3b43cb73bfa86b0dd79
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-08-22 18:44:06 -0400

    styles(web):change generic, imports and home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import { useCountriesContext } from "../context/countries-context";
 import Header from "../components/container/header";
+import CountryCard from "../components/country-card";
 
 function Home() {
   let { saved } = useCountriesContext();
-
+  console.log(saved);
   return (
     <article className="bg-shadow">
       <Header />
-      {saved?.map((e) => (
-        <div key={e.name.common}>
-          <p>{e.name.common}</p>
-        </div>
-      ))}
+      <article className="main default-size">
+        <section className="main__list-of-countries">
+          {saved?.map((e) => (
+            <CountryCard
+              key={e.name.common}
+              name={e.name.common}
+              svg={e.flags.svg}
+            />
+          ))}
+        </section>
+      </article>
     </article>
   );
 }
